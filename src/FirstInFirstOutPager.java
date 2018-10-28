41f38921392f98b9c4cf2d30340834d1146f322f
yuxuanli <lyx.stella@hotmail.com>
2018-10-28 12:51:06 -0700

    first check

---
+++ src/FirstInFirstOutPager.java
@@ -0,0 +1,18 @@
+public class FirstInFirstOutPager extends Pager
+{
+    /**@return the index of the page put into memory first
+    -1 if the page is not in the memory table */
+    @Override
+    public int getEvictionIndex()
+    {
+        int processNumber = getMemoryOrdered().poll().number;
+        int i = 0;
+        for (Page p : getMemoryTable())
+        {
+            if (p.number == processNumber)
+                return i;
+            ++i;
+        }
+        return -1; // this should never happen
+    }
+}
